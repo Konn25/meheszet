138ac4616e2076dc88a55ba7cf73073b97315935
Konn25 <koncz.bence9@gmail.com>
2022-06-12 14:07:08 +0200

    modify

--- meheszet/meheszet_app/views.py
+++ meheszet/meheszet_app/views.py
@@ -1,8 +1,10 @@
-from django.http import JsonResponse, HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import AddUser
 
 from django.contrib import messages
+
+import hashlib as hl
 
 
 # Create your views here.
@@ -18,15 +20,21 @@ def registration(request):
         username = request.POST['username']
         password = request.POST['password']
         email = request.POST['email']
-        beekepingNature = request.POST['beekepingNature']
-        smallProducerRegNumber = request.POST['smallProducerRegNumber']
 
-        if request.POST.get('firstname') is not None:
-            messages.error(request, "Nem jól lett kitöltve")
+        if len(request.POST.getlist('firstname')) == "":
+            messages.error(request, "Add meg a vezetéknevet!")
+        elif len(request.POST.getlist('lastname')) == "":
+            messages.error(request, "Add meg a keresztnevet!")
+        elif len(request.POST.getlist('username')) == "":
+            messages.error(request, "Add meg a felhasználó nevet!")
+        elif len(request.POST.getlist('email')) == "":
+            messages.error(request, "Add meg az email címet!")
+        elif len(request.POST.getlist('password')) == "":
+            messages.error(request, "Add meg a jelszót!!")
         else:
-            new_user = AddUser(firstname=firstname, lastname=lastname, username=username, password=password,
-                               email=email,
-                               beekepingNature=beekepingNature, smallProducerRegNumber=smallProducerRegNumber)
+            new_user = AddUser(firstname=firstname, lastname=lastname, username=username,
+                               password=hl.sha256(password.encode('utf-8')).hexdigest(),
+                               email=email)
             new_user.save()
             return redirect('index')
 
