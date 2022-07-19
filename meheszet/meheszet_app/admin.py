796b33d3632d58153f247c7014b4cb862d444a74
Konn25 <koncz.bence9@gmail.com>
2022-07-19 09:05:31 +0200

    add breeding model

--- meheszet/meheszet_app/admin.py
+++ meheszet/meheszet_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import AddUser
+from .models import AddUser,Breeding
 # Register your models here.
 
 admin.site.register(AddUser)
+admin.site.register(Breeding)
 
